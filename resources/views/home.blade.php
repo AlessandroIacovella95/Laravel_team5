7e8f01806d1e4bb41762ecee324149c135f15a05
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-24 17:57:12 +0200

    fix bug login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('main-content')
-  <section class="container mt-5">
-    <h1>Progetti</h1>
-  </section>
+    <section class="container mt-5">
+        <h1>Homepage</h1>
+    </section>
 @endsection
