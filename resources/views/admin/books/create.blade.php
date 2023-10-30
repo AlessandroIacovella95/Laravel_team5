31ff956ae32b0ea7e6d88804c9c37e6f7e40f6b5
filippo199600 <pippotartaglia@gmail.com>
2023-10-30 19:10:53 +0100

    edit stile view create

--- resources/views/admin/books/create.blade.php
+++ resources/views/admin/books/create.blade.php
@@ -18,56 +18,63 @@
         <form action="{{ route('admin.books.store') }}" method="POST">
             @csrf
             {{-- Titolo --}}
-            <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
-                value="{{ old('title') }}" />
-            @error('title')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+            <div class="col-12 mt-3"> <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                    name="title" value="{{ old('title') }}" />
+                @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
             {{-- Autore --}}
-            <label for="author" class="form-label">Autore</label>
-            <input type="text" class="form-control @error('author') is-invalid @enderror" id="author" name="author"
-                value="{{ old('author') }}" />
-            @error('author')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+            <div class="col-12 mt-3"> <label for="author" class="form-label">Autore</label>
+                <input type="text" class="form-control @error('author') is-invalid @enderror" id="author"
+                    name="author" value="{{ old('author') }}" />
+                @error('author')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
             {{-- Genere --}}
-            <label for="genre_id" class="form-label">Genere</label>
-            <select name="genre_id" id="genre_id" class="form-select @error('genre_id') is-invalid @enderror">
-                <option value="">Nessun genere</option>
-                @foreach ($genres as $genre)
-                    <option value="{{ $genre->id }}" @if (old('genre_id') == $genre->id) selected @endif>
-                        {{ $genre->label }}
-                    </option>
-                @endforeach
-            </select>
-            @error('genre_id')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
+            <div class="col-12 mt-3"> <label for="genre_id" class="form-label">Genere</label>
+                <select name="genre_id" id="genre_id" class="form-select @error('genre_id') is-invalid @enderror">
+                    <option value="">Nessun genere</option>
+                    @foreach ($genres as $genre)
+                        <option value="{{ $genre->id }}" @if (old('genre_id') == $genre->id) selected @endif>
+                            {{ $genre->label }}
+                        </option>
+                    @endforeach
+                </select>
+                @error('genre_id')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
             {{-- Anno di pubblicazione --}}
-            <label for="publication_year" class="form-label">Anno di pubblicazione</label>
-            <input type="number" class="form-control @error('publication_year') is-invalid @enderror" id="publication_year"
-                name="publication_year" min="1500" max="2023" value="{{ old('publication_year') }}" />
-            @error('publication_year')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+            <div class="col-12 mt-3"> <label for="publication_year" class="form-label">Anno di pubblicazione</label>
+                <input type="number" class="form-control @error('publication_year') is-invalid @enderror"
+                    id="publication_year" name="publication_year" min="1500" max="2023"
+                    value="{{ old('publication_year') }}" />
+                @error('publication_year')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
             {{-- Prezzo --}}
-            <label for="price" class="form-label">Prezzo</label>
-            <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
-                value="{{ old('price') }}" />
-            @error('price')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+            <div class="col-12 mt-3"> <label for="price" class="form-label">Prezzo</label>
+                <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"
+                    name="price" value="{{ old('price') }}" />
+                @error('price')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
             {{-- Descrizione --}}
-            <label for="abstract" class="form-label">Descrizione</label>
-            <input type="text" class="form-control @error('abstract') is-invalid @enderror" id="abstract"
-                name="abstract" value="{{ old('abstract') }}" />
-            @error('abstract')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <button type="submit" class="btn btn-primary mt-3">Salva</button>
+            <div class="col-12 mt-3"> <label for="abstract" class="form-label">Descrizione</label>
+                <input type="text" class="form-control @error('abstract') is-invalid @enderror" id="abstract"
+                    name="abstract" value="{{ old('abstract') }}" />
+                @error('abstract')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary my-3">Salva</button>
         </form>
     </div>
 @endsection
